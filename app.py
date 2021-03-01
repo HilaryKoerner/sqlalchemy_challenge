7a9d7977b31e52a48694c7eae6854e59616fc797
Hilary <hilary.koerner@gmail.com>
2021-02-28 19:12:33 -0600

    app.py lists

--- app.py
+++ app.py
@@ -1,5 +1,36 @@
+#import SQL mods
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 #import flask
 from flask import Flask, jsonify
+
+############
+#database setup
+#################
+
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Stations = Base.classes.station
+
+#create an app and pass __name__
+app = Flask(__name__)
+
+#create date and prcp dictionary
+nan = 'nan'
+
+prcp_dict = {}
 
 #create an app and pass __name__
 app = Flask(__name__)
@@ -7,6 +38,7 @@ app = Flask(__name__)
 #index route
 @app.route("/")
 def home():
+    
     print("Server received requst for 'Home' page...")
     return (
         f"Welcome to my SQL Alchemy API<br/>"
@@ -19,18 +51,53 @@ def home():
 #/api/v1.0/precipitation route
 @app.route("/api/v1.0/precipitation")
 def precip():
-    return('test precip')
+    #create session route (link from python to DB)
+    session = Session(engine)
+
+    prcp_info = session.query(Measurement.date, Measurement.prcp).all()
+    #always close the session after the query
+    session.close()
+
+    #unpack the tuples
+    prcp_list = list(np.ravel(prcp_info))
+
+    return jsonify(prcp_list)
 
 
 #/api/v1.0/station route
 @app.route("/api/v1.0/station")
 def station():
-    return('test station')
+    #create session route (link from python to DB)
+    session = Session(engine)
+
+    stations = session.query(Stations.station).all()
+    #always close the session after the query
+    session.close()
+
+    #unpack the tuples
+    station_list = list(np.ravel(stations))
+
+    return jsonify(station_list)
 
 #/api/v1.0/tobs route
 @app.route("/api/v1.0/tobs")
 def tobs():
-    return('test tobs')
+    #create session route (link from python to DB)
+    session = Session(engine)
+
+    station_data = session.query(Measurement.tobs).\
+    filter(Measurement.station == 'USC00519281').\
+    order_by((Measurement.date).desc()).all()
+    #always close the session after the query
+    session.close()
+
+    #unpack the tuples
+    #station_data_list = list(np.ravel(station_data))
+    station_data_list = [result[0] for result in station_data]
+
+    return jsonify(station_data_list)
+
+#merge two tables
 
 #always end with this
 if __name__ == "__main__":
